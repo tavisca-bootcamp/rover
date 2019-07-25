241d863baa6f2df66b1f9283ad1c5435096a5b4b
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-07-25 10:18:58 +0530

    Implemented IMovement and all its types

---
+++ Rover/ForwardMovement.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rover
+{
+    public class ForwardMovement : IMovement
+    {
+        void IMovement.Update(Map roverMap)
+        {
+            int yCoordinate= roverMap.CurrentLocation.YCoordinate, xCoordinate= roverMap.CurrentLocation.XCoordinate;
+            if (roverMap.MapCompass.Needle == Direction.North)
+                yCoordinate += 1;
+            if (roverMap.MapCompass.Needle == Direction.South)
+                yCoordinate -= 1;
+            if (roverMap.MapCompass.Needle == Direction.West)
+                xCoordinate -= 1;
+            if (roverMap.MapCompass.Needle == Direction.East)
+                xCoordinate += 1;
+            if (roverMap.TotalArea.HasLocation(new Location(xCoordinate, yCoordinate)))
+                roverMap.CurrentLocation = new Location(xCoordinate, yCoordinate);
+        }
+    }
+}
---
+++ Rover/RotateLeftMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rover
+{
+    public class RotateLeftMovement : IRotation
+    {
+        void IMovement.Update(Map roverMap)
+        {
+            if (roverMap.MapCompass.Needle == Direction.North)
+                roverMap.MapCompass.Needle = Direction.West;
+            else if (roverMap.MapCompass.Needle == Direction.South)
+                roverMap.MapCompass.Needle = Direction.East;
+            else if (roverMap.MapCompass.Needle == Direction.West)
+                roverMap.MapCompass.Needle = Direction.South;
+            else
+                roverMap.MapCompass.Needle = Direction.North;
+        }
+    }
+}
