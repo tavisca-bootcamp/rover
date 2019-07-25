7ba354b8b0c83ae199b45aef3ecc8380da2f4f39
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-07-25 17:01:45 +0530

    Added more tests

--- Rover.Tests/MarsRoverFixture.cs
+++ Rover.Tests/MarsRoverFixture.cs
@@ -16,6 +16,16 @@ namespace Rover.Tests
         {
             new MarsRover().MarsRoverSentToMars("5 5", "3 3 E", "MMRRMMRRMRRRRM").Should().Be("5 1 E");
         }
+        [Fact]
+        public void Rover_Should_in_both_axis_if_facing_NE_Test()
+        {
+            new MarsRover().MarsRoverSentToMars("5 5", "3 3 NE", "MM").Should().Be("5 5 NE");
+        }
+        [Fact]
+        public void Rover_rotates_by_45_degrees_Test()
+        {
+            new MarsRover().MarsRoverSentToMars("5 5", "3 3 NE", "R").Should().Be("3 3 E");
+        }
 
     }
 }
