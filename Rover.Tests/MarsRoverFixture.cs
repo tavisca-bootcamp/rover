4e637adb100a202fda5d4d0c2988378d882e5384
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-07-25 16:56:47 +0530

    New test are implenented to test the whole system

---
+++ Rover.Tests/MarsRoverFixture.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+using FluentAssertions;
+using Rover;
+namespace Rover.Tests
+{
+    public class MarsRoverFixture
+    {
+        [Fact]
+        public void Rover1_Test()
+        {
+            new MarsRover().MarsRoverSentToMars("5 5", "1 2 N", "LLMLLMLLMLLMM").Should().Be("1 3 N");
+        }
+        [Fact]
+        public void Rover2_Test()
+        {
+            new MarsRover().MarsRoverSentToMars("5 5", "3 3 E", "MMRRMMRRMRRRRM").Should().Be("5 1 E");
+        }
+
+    }
+}
---
+++ Rover/MarsRover.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rover
+{
+    public class MarsRover
+    {
+        public string MarsRoverSentToMars(string input1,string input2,string instructions)
+        {
+            string[] locationData = input2.Split(' ');
+            string[] surfaceAreaData= input1.Split(' ');
+            Location roverLocation = new Location(Int32.Parse(locationData[0]),Int32.Parse(locationData[1]));
+            SurfaceArea marsSurface = new SurfaceArea(new Location(Int32.Parse(surfaceAreaData[0]), Int32.Parse(surfaceAreaData[1])));
+            Compass roverCompass = new Compass(locationData[2]);
+            Map marsMap = new Map(roverLocation,marsSurface,roverCompass);
+            Rover marsRover = new Rover(marsMap);
+            marsRover.ReadSignals(instructions);
+            return marsRover.SendLocationDetails();
+        }
+    }
+}
